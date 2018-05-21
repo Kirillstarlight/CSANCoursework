3242383dbef33370f89995d0cca4294b35abd508
kirillstarlight <kirillstarlight@gmail.com>
2018-05-21 10:25:41 +0300

    test

---
+++ ClientProject/NetworkMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongGame
+{
+    public class NetworkMessage
+    {
+        public IPEndPoint Sender { get; set; }
+        public Packet Packet { get; set; }
+        public DateTime ReceiveTime { get; set; }
+    }
+}
---
+++ ServerProject/PlayerInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using PongGame;
+
+namespace ServerProject
+{
+    public class PlayerInfo
+    {
+        public Paddle Paddle;
+        public IPEndPoint Endpoint;
+        public DateTime LastPacketReceivedTime = DateTime.MinValue;     // From Server Time
+        public DateTime LastPacketSentTime = DateTime.MinValue;         // From Server Time
+        public long LastPacketReceivedTimestamp = 0;                    // From Client Time
+        public bool HavePaddle = false;
+        public bool Ready = false;
+
+        public bool IsSet
+        {
+            get { return Endpoint != null; }
+        }
+    }
+}
